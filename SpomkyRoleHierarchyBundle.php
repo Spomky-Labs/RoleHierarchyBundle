88d4576528182b286e85d516b1959c040d39418b
Spomky <spomky+git@gmail.com>
2013-11-28 19:44:09 +0100

    Unused Statement

--- SpomkyRoleHierarchyBundle.php
+++ SpomkyRoleHierarchyBundle.php
@@ -2,7 +2,6 @@
 
 namespace Spomky\RoleHierarchyBundle;
 
-use Spomky\RoleHierarchyBundle\DependencyInjection\SpomkyRoleHierarchyExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SpomkyRoleHierarchyBundle extends Bundle
